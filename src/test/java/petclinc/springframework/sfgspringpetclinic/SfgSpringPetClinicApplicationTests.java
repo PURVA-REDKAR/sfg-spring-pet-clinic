8841128903197b107258c809add327929748e732
Purva <purva.red@gmail.com>
2020-01-05 00:00:45 -0500

    Initial commit

---
+++ src/test/java/petclinc/springframework/sfgspringpetclinic/SfgSpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package petclinc.springframework.sfgspringpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgSpringPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
