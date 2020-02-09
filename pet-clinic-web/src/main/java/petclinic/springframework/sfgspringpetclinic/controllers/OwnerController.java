a88abe37044fd9d9cb77426bfbc5103d69eca234
Purva <purva.red@gmail.com>
2020-02-09 04:03:34 -0500

    Owner list display. closes #15

--- pet-clinic-web/src/main/java/petclinic/springframework/sfgspringpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/petclinic/springframework/sfgspringpetclinic/controllers/OwnerController.java
@@ -1,12 +1,22 @@
 package petclinic.springframework.sfgspringpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import petclinic.springframework.sfgspringpetclinic.services.OwnerService;
 
 @Controller
 public class OwnerController {
+
+    private final OwnerService ownerServicer;
+
+    public OwnerController(OwnerService ownerServicer) {
+        this.ownerServicer = ownerServicer;
+    }
+
     @RequestMapping({"/owner","owner/index", "owner/index.html"})
-    public String owner(){
+    public String owner(Model model){
+        model.addAttribute("owners",ownerServicer.findAll());
         return "owner/index";
     }
 }
